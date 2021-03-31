a344817ffb719e569aeedecc55ec683c3fc0a4ab
velocicraptor <65057624+velocicraptor@users.noreply.github.com>
2021-03-31 14:38:32 +0530

    Add files via upload

---
+++ CabBookingApplication/src/com/cg/mts/entities/Admin.java
@@ -0,0 +1,14 @@
+package com.cg.mts.entities;
+
+public class Admin extends AbstractUser {
+	private int adminId;
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	
+}
