baa8d538b473e7b017c4249f7896b54d680d2b61
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-01 09:10:53 +0530

    Update Driver.java

--- CabBookingApplication/src/com/cg/mts/entities/Driver.java
+++ CabBookingApplication/src/com/cg/mts/entities/Driver.java
@@ -5,4 +5,29 @@ public class Driver extends AbstractUser {
 	private String licenseNo;
 	private Cab cab;
 	private float rating;
+	public int getDriverId() {
+		return driverId;
+	}
+	public void setDriverId(int driverId) {
+		this.driverId = driverId;
+	}
+	public String getLicenseNo() {
+		return licenseNo;
+	}
+	public void setLicenseNo(String licenseNo) {
+		this.licenseNo = licenseNo;
+	}
+	public Cab getCab() {
+		return cab;
+	}
+	public void setCab(Cab cab) {
+		this.cab = cab;
+	}
+	public float getRating() {
+		return rating;
+	}
+	public void setRating(float rating) {
+		this.rating = rating;
+	} 
+
 }
