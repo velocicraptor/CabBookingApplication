7619ef053ad59e3d7b815185d91d782d0f58f0ca
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-01 09:10:03 +0530

    Update Customer.java

--- CabBookingApplication/src/com/cg/mts/entities/Customer.java
+++ CabBookingApplication/src/com/cg/mts/entities/Customer.java
@@ -1,5 +1,14 @@
 package com.cg.mts.entities;
 
-public class Customer extends AbstractUser {
+public class Customer  extends AbstractUser {
 	private int customerId;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	} 
+
 }
