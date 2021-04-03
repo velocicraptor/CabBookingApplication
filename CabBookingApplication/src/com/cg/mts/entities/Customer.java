8f4c7f48f7491ce17256878a6f38b13ddabf4864
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-03 17:11:10 +0530

    Update Customer.java

--- CabBookingApplication/src/com/cg/mts/entities/Customer.java
+++ CabBookingApplication/src/com/cg/mts/entities/Customer.java
@@ -1,14 +1,16 @@
 package com.cg.mts.entities;
+import javax.persistence.Entity;
 
-public class Customer  extends AbstractUser {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends AbstractUser {
 	private int customerId;
 
-	public int getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	} 
 
 }
