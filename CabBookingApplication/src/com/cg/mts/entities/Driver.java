e279af80549e90668b82d641187a70625ffddced
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-03 17:14:57 +0530

    Update Driver.java

--- CabBookingApplication/src/com/cg/mts/entities/Driver.java
+++ CabBookingApplication/src/com/cg/mts/entities/Driver.java
@@ -1,33 +1,20 @@
 package com.cg.mts.entities;
+import javax.persistence.Entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Driver extends AbstractUser {
 	private int driverId;
 	private String licenseNo;
 	private Cab cab;
 	private float rating;
-	public int getDriverId() {
-		return driverId;
-	}
-	public void setDriverId(int driverId) {
-		this.driverId = driverId;
-	}
-	public String getLicenseNo() {
-		return licenseNo;
-	}
-	public void setLicenseNo(String licenseNo) {
-		this.licenseNo = licenseNo;
-	}
-	public Cab getCab() {
-		return cab;
-	}
-	public void setCab(Cab cab) {
-		this.cab = cab;
-	}
-	public float getRating() {
-		return rating;
-	}
-	public void setRating(float rating) {
-		this.rating = rating;
-	} 
-
+	
+	
 }
