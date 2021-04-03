26126393306efa2d26dc32583949498543e50390
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-03 17:10:05 +0530

    Update Admin.java

--- CabBookingApplication/src/com/cg/mts/entities/Admin.java
+++ CabBookingApplication/src/com/cg/mts/entities/Admin.java
@@ -1,16 +1,17 @@
 package com.cg.mts.entities;
+import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin extends AbstractUser {
 	private int adminId;
-
-	public int getAdminId() {
-		return adminId;
-	}
-
-	public void setAdminId(int adminId) {
-		this.adminId = adminId;
-	}
-
 	
 
+	
 }
