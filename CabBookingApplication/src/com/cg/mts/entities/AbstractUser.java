6946ac5f11876a229cdb58f1e287e2e29d1fe0a0
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-03 17:09:38 +0530

    Update AbstractUser.java

--- CabBookingApplication/src/com/cg/mts/entities/AbstractUser.java
+++ CabBookingApplication/src/com/cg/mts/entities/AbstractUser.java
@@ -1,49 +1,21 @@
 package com.cg.mts.entities;
 
-public class AbstractUser {
-	
-	public AbstractUser() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
+import javax.persistence.Entity;
 
-	public AbstractUser(String username, String password, String mobileNumber, String email) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.mobileNumber = mobileNumber;
-		this.email = email;
-	}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 
+public abstract class AbstractUser {
 	private String username;
 	private String password;
 	private String mobileNumber;
 	private String email;
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 
 }
